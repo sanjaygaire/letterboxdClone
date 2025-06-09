e328fceff6ba422d24d6f993e6284e1e1eb39cd3
sanjaygaire <gairesanjay59@gmail.com>
2025-06-09 14:50:24 +0545

    yup

--- frontend/app/src/components/Movie.jsx
+++ frontend/app/src/components/Movie.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 
-function Movie() {
+function Movie(props) {
   return (
-    <div className='bg-white text-black max-w-40 m-2 max-h-70 p-1 justify-between border-2 border-black hover:bg-green-600 hover:text-white'>
+    <div className='bg-white text-black max-w-40 m-2 max-h-70 p-1 rounded justify-between border-2 border-black hover:bg-green-600 hover:text-white'>
       <div className='w-full h-40 bg-gray-800 flex items-center justify-center overflow-hidden'>
         <img
-          src="https://filmartgallery.com/cdn/shop/files/Inception-Vintage-Movie-Poster-Original.jpg?v=1738912645"
+          src={props.poster}
           className='w-full h-full object-cover'
           alt="movie poster"
         />
       </div>
       <div>
-        <p className='font-semibold text-sm mt-1'>Inception</p>
+        <p className='font-semibold text-sm mt-1'>{props.name}</p>
         <div className='flex justify-between text-xs text-gray-700 mt-1'>
-          <p>⭐ 4.5</p>
-          <p>Watched ✅</p>
+          <p>⭐ {props.rating}</p>
+          <p>{props.watched ? "Watched" : "Not Watched"} </p>
         </div>
       </div>
     </div>
